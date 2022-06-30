6f208951885330fde7280dcf5e2b2d11868193d3
shrn-kira <shrnkira@gmail.com>
2022-06-30 15:58:32 -0700

    a program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
+
 /**
 *  main - multiplies two positive numbers
 *  @argc: number of arguments
