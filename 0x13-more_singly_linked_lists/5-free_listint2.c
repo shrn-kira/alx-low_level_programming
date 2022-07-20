cc1fc5db2509204e9aaa26c2896ee66d859aaa5f
shrn-kira <shrnkira@gmail.com>
2022-07-20 01:15:19 +0000

    a function that frees a listint_t list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,12 +8,17 @@
 */
 void free_listint2(listint_t **head)
 {
+	listint_t *temp;
+
 	if (head == NULL)
 		return;
+
 	while (*head)
 	{
+		temp = (*head)->next;
 		free(*head);
-		*head = (*head)->next;
+		*head = temp;
 	}
-	head = NULL;
+
+	*head = NULL;
 }
